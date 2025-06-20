52bbd60e55fcdaef300aa92fafe6968c04d3cff8
adibahnordin98 <adibahnordin11@gmail.com>
2025-06-20 11:54:20 +0000

    Recovered changes after disk fix

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ _ = load_dotenv()
 def initialize_agent(
     prompt_file,
     tools_to_use=None,
-    model_dir="/model-weights",
+    model_dir="models",
     temp_dir="temp",
     device="cuda",
     model="chatgpt-4o-latest",
@@ -105,8 +105,8 @@ if __name__ == "__main__":
         "ChestXRaySegmentationTool",
         "ChestXRayReportGeneratorTool",
         "XRayVQATool",
-        # "LlavaMedTool",
-        # "XRayPhraseGroundingTool",
+        "LlavaMedTool",
+        "XRayPhraseGroundingTool",
         # "ChestXRayGeneratorTool",
     ]
 
@@ -121,7 +121,7 @@ if __name__ == "__main__":
     agent, tools_dict = initialize_agent(
         "medrax/docs/system_prompts.txt",
         tools_to_use=selected_tools,
-        model_dir="/model-weights",  # Change this to the path of the model weights
+        model_dir="models",  # Change this to the path of the model weights
         temp_dir="temp",  # Change this to the path of the temporary directory
         device="cuda",  # Change this to the device you want to use
         model="gpt-4o",  # Change this to the model you want to use, e.g. gpt-4o-mini
